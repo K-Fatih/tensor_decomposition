e74476356c544ab968d9049216c856267d91d75c
FatihK <st165993@stud.uni-stuttgart.de>
2021-07-07 13:57:33 +0200

    norms calculated with list

--- TensorDecomp.py
+++ TensorDecomp.py
@@ -4,19 +4,19 @@ import matplotlib.pyplot as plt
 
 from numpy.linalg import svd
 from scipy.linalg import norm
-from scipy.linalg import clarkson_woodruff_transform
 from tensorly.decomposition import parafac
 from tensorly.decomposition import tucker
 from tensorly.decomposition import matrix_product_state
-from timeit import default_timer as timer
+from scipy.linalg import clarkson_woodruff_transform
 from sklearn.decomposition import NMF
-from tensorly.contrib.sparse.decomposition import tucker as sparse_tucker
+from timeit import default_timer as timer
 
 
-decomp_list = ['svd', 'parafac', 'tucker', 'matrix_product_state', 'NMF','sparse_tucker', 'clarkson_woodruff_transform']
+
+decomp_list = ['svd', 'parafac', 'tucker', 'matrix_product_state', 'NMF', 'clarkson_woodruff_transform']
 
 
-class TensorDecomp:
+class TensorDecomp():
     """
     A class to represent a tensor object with decomposition, reconstruction, error methods.
 
@@ -50,6 +50,11 @@ class TensorDecomp:
 
     """
     def __init__(self, tensor):
+        """Creates the TensorDecomp object with given numpy.ndarray object.
+
+        Args:
+            tensor (numpy.ndarray): The tensor to be decomposed.
+        """
 
         self.tensor = tensor      
         self.decMemSize = 0
@@ -62,9 +67,8 @@ class TensorDecomp:
         Computes the decomposition time and assigns to self.decomp_time.
         Assigns the func argument as the decomposition type to self.decomp_type.
 
-        Parameters
+        Args:
         ----------
-        self    : object of class TensorDecomp type.
         func    : the decomposition type. e.g.: parafac, NMF etc.
         *args   : passed arguments.
         **kwargs: passed keyword arguments. e.g.: rank, sketch_size etc.
@@ -87,14 +91,17 @@ class TensorDecomp:
             self.decomp_type = func.__name__
 
         elif func.__name__ == 'NMF':
-            self.nmf_obj = NMF()
-            ts = timer()
-            self.recons = []
-            self.recons.append(self.nmf_obj.fit_transform(self.tensor) )
-            self.recons.append(self.nmf_obj.components_)
-            te = timer()
-            self.decomp_time = te-ts
-            self.decomp_type = func.__name__
+            try:
+                self.nmf_obj = NMF(**kwargs)
+                ts = timer()
+                self.recons = []
+                self.recons.append(self.nmf_obj.fit_transform(self.tensor) )
+                self.recons.append(self.nmf_obj.components_)
+                te = timer()
+                self.decomp_time = te-ts
+                self.decomp_type = func.__name__
+            except:
+                raise
 
         elif args:
             ts = timer()
@@ -126,10 +133,10 @@ class TensorDecomp:
         Reconstructs the recons TensorDecomp object.
         Assigns the reconstructed tensor to self.recons attribute.
 
-        Parameters
+        Args:
         ----------
-        self : object of class TensorDecomp type.
-            
+        None
+
         Returns
         -------
         None            
@@ -137,7 +144,11 @@ class TensorDecomp:
         """
 
         if self.decomp_type == 'svd':
-            self.recons = self.recons[0] @ (np.diag(self.recons[1])@self.recons[2])
+            if self.tensor.ndim < 3:
+                self.recons = self.recons[0][...,:self.tensor.shape[-1]]*self.recons[1]@self.recons[2]
+            else:
+                self.recons = np.matmul(self.recons[0][...,:self.tensor.shape[-1]], self.recons[1][..., None] * self.recons[2])
+
 
         elif self.decomp_type == 'NMF':
             self.recons = self.nmf_obj.inverse_transform(self.recons[0])
@@ -157,11 +168,11 @@ class TensorDecomp:
         elif self.decomp_type == 'clarkson_woodruff_transform':
             self.recons = self.recons
 
-    def error(self,func, x, y):
+    def error(self,func, x, y, *args, **kwargs):
         """
         Computes the error between the original and reconstructed tensor with a given error function.
 
-        Parameters
+        Args:
         ----------
         func    : function object for error calculation. Example: np.linalg.norm
         x       : the original tensor
@@ -173,15 +184,26 @@ class TensorDecomp:
             the error between the original and the reconstructed tensor.            
         
         """
+        if self.decomp_type == 'clarkson_woodruff_transform':
+            print("Sketching decomposition error cannot be calculated.")
+            return None
+        else:
+            return (func(x-y)) / func(x)
 
-        return (func(x-y)) / func(x)
+def errList(tensor, decompMet, vectorR, vectorL, MatrixR, MatrixL, normL, rank = None, **kwargs):
+    """A function to calculate the norm of the followging:
 
-def errList(tensor, decompMet, vectorL, vectorR, MatrixL, MatrixR, uNorm = [], rank = [2,3]):
-    """A function to calculate the Frobenius, L1, L_inf, and Spectral-Norm of
-    the tensor decomposition, Tensor @ Vector, Vector @ Tensor, Tensor @ Matrix0, Matrix @ Tensor, 
-    Vector.T @ Tensor @ Vector, Matrix.T @ Tensor @ Matrix.    
+    tensor decomposition, 
+    Tensor @ Vector, 
+    Vector @ Tensor, 
+    Tensor @ Matrix, 
+    Matrix @ Tensor, 
+    Vector.T @ Tensor @ Vector, 
+    Matrix.T @ Tensor @ Matrix operations error, 
 
-    Parameters
+    in this order.    
+
+    Args:
     ----------
     tensor (TensorDecomp)   : TensorDecomp object.
     decompMet (Function)    : The decomposition method to decompose the TensorDecomp object.
@@ -189,34 +211,36 @@ def errList(tensor, decompMet, vectorL, vectorR, MatrixL, MatrixR, uNorm = [], r
     vectorR (numpy.ndarray) : A vector of size tensor.tensor.shape([-1])
     MatrixL (numpy.ndarray) : A matrix of size tensor.tensor.shape([:-1])
     MatrixR (numpy.ndarray) : A matrix of size tensor.tensor.shape([-1:-3:-1])
-    uNorm                   : A list of user norm functions. (optional)
-    rank                    : Optional argument to decompose with the relatead rank. (optional)
+    normL (list)            : A list of norm functions to calculate the norm of the errors.
+    rank (int or list)      : To decompose with the relatead rank. (optional)
 
     Returns
     -------
 
-    A list of 7 lists each contains the Frobenius, L1, L_inf, Spectral Norm of errors of Tensor operations.
+    A list of 7 lists each contains the norm of the errors of the above tensor operations.
 
     """
-    tensor.decompose(decompMet, rank)
-    tensor.reconstruct()
-
-    err_fro  = lambda  x, y : norm(x-y, ord='fro') / norm(x, ord='fro') if (x.ndim == 2) else norm(x-y, ord=None) / norm(x, ord=None)
-    err_L1   = lambda  x, y : norm(x-y, ord=1) / norm(x, ord=1) if (x.ndim == 2) else norm(x-y, ord=None) / norm(x, ord=None)
-    err_Linf = lambda  x, y : norm(x-y, ord=inf) / norm(x, ord=inf) if (x.ndim == 2) else norm(x-y, ord=None) / norm(x, ord=None)
-    err_Spec = lambda  x, y : norm(x-y, ord=2) / norm(x, ord=2) if (x.ndim == 2) else norm(x-y, ord=None) / norm(x, ord=None)
+    if tensor.tensor.ndim != 2 and decompMet in [NMF, clarkson_woodruff_transform] :
+        return (f"It is not possible to decompose {tensor.tensor.ndim = } with the {decompMet.__name__} method!")           
     
-    normL = [err_fro, err_L1, err_Linf, err_Spec]
-    normL.append(*uNorm) if len(uNorm)!=0 else None
+    if decompMet in [NMF,clarkson_woodruff_transform]:
+        tensor.decompose(decompMet, **kwargs)
+        tensor.reconstruct()        
+    else:
+        tensor.decompose(decompMet, rank)
+        tensor.reconstruct()
 
+    decErr = None
 
     tensVec = [norm(tensor.tensor@vectorR, tensor.recons@vectorR) for norm in normL]
     vecTens = [norm(vectorL@tensor.tensor, vectorL@tensor.recons) for norm in normL]
     matLTens = [norm(MatrixL@tensor.tensor, MatrixL@tensor.recons) for norm in normL]
     tensMatR = [norm(tensor.tensor@MatrixR, tensor.recons@MatrixR) for norm in normL]
-    vectTenvec = [norm(vectorL@tensor.tensor@vectorR, vectorL@tensor.recons@vectorR) for norm in normL]
-    matTTenMat = [norm(MatrixL@tensor.tensor@MatrixR, MatrixL@tensor.recons@MatrixR) for norm in normL]
-    decErr = [norm(tensor.tensor, tensor.recons) for norm in normL]
+    vectTensvec = [norm(vectorL@tensor.tensor@vectorR, vectorL@tensor.recons@vectorR) for norm in normL]
+    matTTensMat = [norm(MatrixL@tensor.tensor@MatrixR, MatrixL@tensor.recons@MatrixR) for norm in normL]
 
-    return [decErr, tensVec, vecTens, matLTens, tensMatR, vectTenvec, matTTenMat]
-
+    if decompMet != clarkson_woodruff_transform:
+        decErr = [norm(tensor.tensor, tensor.recons) for norm in normL]
+        return [decErr, tensVec, vecTens, tensMatR, matLTens, vectTensvec, matTTensMat]
+    else:
+        return [decErr, tensVec, vecTens, tensMatR, matLTens, vectTensvec, matTTensMat]
